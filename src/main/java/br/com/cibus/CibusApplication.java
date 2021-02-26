550e7806b16b9c0213d3ba52888ae578cf3c843f
Lucas Peixoto <lucas.peixoto.14@gmail.com>
2021-02-26 15:09:38 -0300

    colocando dependencias que faltavam no projeto para funcionar a jsp

--- src/main/java/br/com/cibus/CibusApplication.java
+++ src/main/java/br/com/cibus/CibusApplication.java
@@ -2,12 +2,18 @@ package br.com.cibus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CibusApplication {
+public class CibusApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CibusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CibusApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CibusApplication.class);
+    }
 }
