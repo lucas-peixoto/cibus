a629015eabb4e90809481b15ddd647e9e12bbf62
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2021-04-06 08:48:59 -0300

    refatorando uso de urls no teste end-to-end do tipo de cozinha

--- src/test/java/br/com/cibus/tipodecozinha/TipoDeCozinhaEndToEndTest.java
+++ src/test/java/br/com/cibus/tipodecozinha/TipoDeCozinhaEndToEndTest.java
@@ -20,7 +20,6 @@ public class TipoDeCozinhaEndToEndTest {
     @LocalServerPort
     private int serverPort;
     private ChromeDriver browser;
-    private String base_url;
 
     @BeforeAll
     static void beforeAll() {
@@ -30,7 +29,6 @@ public class TipoDeCozinhaEndToEndTest {
     @BeforeEach
     void init() {
         browser = new ChromeDriver();
-        base_url = "http://localhost:" + serverPort;
     }
 
     @AfterEach
@@ -40,7 +38,7 @@ public class TipoDeCozinhaEndToEndTest {
 
     @Test
     void lista() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
 
         String headerTitle = browser.getTitle();
         WebElement titulo = browser.findElementByCssSelector(".titulo");
@@ -54,12 +52,12 @@ public class TipoDeCozinhaEndToEndTest {
 
     @Test
     void adiciona() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
         browser.findElementByCssSelector(".link-adicionar-novo-tipo-de-cozinha").click();
 
         String titulo = browser.findElementByCssSelector(".titulo").getText();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/novo");
+        assertThat(browser.getCurrentUrl()).isEqualTo(adicionaURL());
         assertThat(browser.getTitle()).isEqualTo("Adicionar um Tipo de Cozinha");
         assertThat(titulo).isEqualTo("Adicionar um Tipo de Cozinha");
 
@@ -69,38 +67,38 @@ public class TipoDeCozinhaEndToEndTest {
 
         List<WebElement> linhas = browser.findElementsByCssSelector("table.table tbody tr td.nome-tipo-de-cozinha");
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha");
+        assertThat(browser.getCurrentUrl()).isEqualTo(listaURL());
         assertThat(linhas).extracting(WebElement::getText).contains("Azerbaijani");
     }
 
     @Test
     void adicionaComNomeInvalido() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
         browser.findElementByCssSelector(".link-adicionar-novo-tipo-de-cozinha").click();
 
         WebElement form = browser.findElementByCssSelector(".form-adicionar-tipo-de-cozinha");
         WebElement submitButton = form.findElement(By.cssSelector("input[type='submit']"));
         submitButton.click();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/novo");
+        assertThat(browser.getCurrentUrl()).isEqualTo(adicionaURL());
 
         form.findElement(By.id("nome")).sendKeys("Chinesa");
         submitButton.click();
 
         String errors = browser.findElementById("nome.errors").getText();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/novo");
+        assertThat(browser.getCurrentUrl()).isEqualTo(adicionaURL());
         assertThat(errors).contains("Nome já existente");
     }
 
     @Test
     void edita() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
         browser.findElementByCssSelector("table.table tbody tr a.link-editar-tipo-de-cozinha").click();
 
         String titulo = browser.findElementByCssSelector(".titulo").getText();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/editar/1");
+        assertThat(browser.getCurrentUrl()).isEqualTo(editaURL());
         assertThat(browser.getTitle()).isEqualTo("Editar um Tipo de Cozinha");
         assertThat(titulo).isEqualTo("Editar um Tipo de Cozinha");
 
@@ -113,7 +111,7 @@ public class TipoDeCozinhaEndToEndTest {
 
         List<WebElement> linhas = browser.findElementsByCssSelector("table.table tbody tr td.nome-tipo-de-cozinha");
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha");
+        assertThat(browser.getCurrentUrl()).isEqualTo(listaURL());
         assertThat(linhas)
                 .extracting(WebElement::getText)
                 .contains("Mexicana")
@@ -122,7 +120,7 @@ public class TipoDeCozinhaEndToEndTest {
 
     @Test
     void editaComNomeInvalido() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
         browser.findElementByCssSelector("table.table tbody tr a.link-editar-tipo-de-cozinha").click();
 
         WebElement form = browser.findElementByCssSelector(".form-editar-tipo-de-cozinha");
@@ -132,7 +130,7 @@ public class TipoDeCozinhaEndToEndTest {
         inputNome.clear();
         submitButton.click();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/editar/1");
+        assertThat(browser.getCurrentUrl()).isEqualTo(editaURL());
 
         inputNome.clear();
         inputNome.sendKeys("Chinesa");
@@ -140,20 +138,32 @@ public class TipoDeCozinhaEndToEndTest {
 
         String errors = browser.findElementById("nome.errors").getText();
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha/editar/1");
+        assertThat(browser.getCurrentUrl()).isEqualTo(editaURL());
         assertThat(errors).contains("Nome já existente");
     }
 
     @Test
     void remove() {
-        browser.get(base_url + "/admin/tipos-de-cozinha");
+        browser.get(listaURL());
         browser.findElementByCssSelector("table.table tbody tr:last-child .button-remover-tipo-de-cozinha").click();
 
         List<WebElement> linhas = browser.findElementsByCssSelector("table.table tbody tr td.nome-tipo-de-cozinha");
 
-        assertThat(browser.getCurrentUrl()).isEqualTo(base_url + "/admin/tipos-de-cozinha");
+        assertThat(browser.getCurrentUrl()).isEqualTo(listaURL());
         assertThat(linhas)
                 .extracting(WebElement::getText)
                 .doesNotContain("Italiana");
+    }
+
+    private String listaURL() {
+        return "http://localhost:" + serverPort + "/admin/tipos-de-cozinha";
+    }
+
+    private String adicionaURL() {
+        return "http://localhost:" + serverPort + "/admin/tipos-de-cozinha/novo";
+    }
+
+    private String editaURL() {
+        return "http://localhost:" + serverPort + "/admin/tipos-de-cozinha/editar/1";
     }
 }
