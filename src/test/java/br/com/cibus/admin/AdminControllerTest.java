7a20e0dcf78a0a030c5e25b1f91c08e5d15b259a
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2021-05-05 12:48:45 -0300

    adicionando teste de controller para o dashboard admin

---
+++ src/test/java/br/com/cibus/admin/AdminControllerTest.java
@@ -0,0 +1,35 @@
+package br.com.cibus.admin;
+
+import br.com.cibus.tipodecozinha.TipoDeCozinhaRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AdminController.class)
+public class AdminControllerTest {
+
+    @MockBean
+    private TipoDeCozinhaRepository tipoDeCozinhaRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void dashboard() throws Exception {
+        List<RestaurantesPorTipoDeCozinha> restaurantesPorTipoDeCozinha = List.of();
+        when(tipoDeCozinhaRepository.contaRestaurantesPorTipoDeCozinha()).thenReturn(restaurantesPorTipoDeCozinha);
+
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("restaurantesPorTipoDeCozinha", restaurantesPorTipoDeCozinha))
+                .andExpect(view().name("admin/dashboard"));
+    }
+}
