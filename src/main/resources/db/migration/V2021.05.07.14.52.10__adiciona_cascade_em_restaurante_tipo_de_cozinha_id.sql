59c768fd36f07c1841d4bc8e247d8fe2905c1653
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2021-05-12 23:36:40 -0300

    removendo migration de cascade na chave estrangeira tipo_de_cozinha_id

--- src/main/resources/db/migration/V2021.05.07.14.52.10__adiciona_cascade_em_restaurante_tipo_de_cozinha_id.sql
+++
@@ -1,9 +0,0 @@
-alter table restaurante
-    drop constraint fk_tipo_de_cozinha_id;
-
-alter table restaurante
-    add constraint fk_tipo_de_cozinha_id
-        foreign key (tipo_de_cozinha_id)
-        references tipo_de_cozinha (id)
-        on delete cascade
-        on update cascade;
