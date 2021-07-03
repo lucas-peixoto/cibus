c1b9579b50e60637db63df39a15afe8892245541
Lucas Peixoto de Alencar Rocha <lucas.peixoto.14@gmail.com>
2021-07-03 14:54:58 -0300

    validando valor minimo e maximo do tempo de entrega do restaurante

--- src/main/java/br/com/cibus/restaurante/RestauranteParaEdicaoValidator.java
+++ src/main/java/br/com/cibus/restaurante/RestauranteParaEdicaoValidator.java
@@ -21,6 +21,10 @@ public class RestauranteParaEdicaoValidator implements Validator {
     public void validate(Object target, Errors errors) {
         RestauranteParaEdicaoForm restauranteParaEdicaoForm = (RestauranteParaEdicaoForm) target;
 
+        if (restauranteParaEdicaoForm.getTempoMinimoEntrega() > restauranteParaEdicaoForm.getTempoMaximoEntrega()) {
+            errors.rejectValue("tempoMinimoEntrega", "tempoMinimoEntrega.maior.que.maximo", "Tempo mínimo de entrega não pode ser maior que o tempo máximo de entrega");
+        }
+
         if (restauranteRepository.existsByNomeAndIdNot(restauranteParaEdicaoForm.getNome(), restauranteParaEdicaoForm.getId())) {
             errors.rejectValue("nome", "nome.ja.existente", "Nome já existente");
         }
--- src/test/java/br/com/cibus/restaurante/RestauranteParaEdicaoValidatorTest.java
+++ src/test/java/br/com/cibus/restaurante/RestauranteParaEdicaoValidatorTest.java
@@ -25,6 +25,26 @@ class RestauranteParaEdicaoValidatorTest {
     }
 
     @Test
+    void quandoTempoMinimoDeEntregaForMenorQueMaximoNaoDeveDarErro() {
+        when(restauranteParaEdicaoForm.getTempoMinimoEntrega()).thenReturn(10);
+        when(restauranteParaEdicaoForm.getTempoMaximoEntrega()).thenReturn(15);
+
+        restauranteParaEdicaoValidator.validate(restauranteParaEdicaoForm, errors);
+
+        verify(errors, never()).rejectValue("tempoMinimoEntrega", "tempoMinimoEntrega.maior.que.maximo", "Tempo mínimo de entrega não pode ser maior que o tempo máximo de entrega");
+    }
+
+    @Test
+    void quandoTempoMinimoDeEntregaForMaiorQueMaximoDeveDarErro() {
+        when(restauranteParaEdicaoForm.getTempoMinimoEntrega()).thenReturn(15);
+        when(restauranteParaEdicaoForm.getTempoMaximoEntrega()).thenReturn(10);
+
+        restauranteParaEdicaoValidator.validate(restauranteParaEdicaoForm, errors);
+
+        verify(errors).rejectValue("tempoMinimoEntrega", "tempoMinimoEntrega.maior.que.maximo", "Tempo mínimo de entrega não pode ser maior que o tempo máximo de entrega");
+    }
+
+    @Test
     void quandoNomeNaoExisteMasIdExisteNaoDeveDarErro() {
         when(restauranteParaEdicaoForm.getId()).thenReturn(2L);
         when(restauranteParaEdicaoForm.getNome()).thenReturn("Novo Almanara");
